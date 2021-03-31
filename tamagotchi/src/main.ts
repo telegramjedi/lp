e7be117211c0b63d6f506a8f0328387743352de2
th3exorcist <ghostsavestheworld@gmail.com>
2021-03-31 15:56:39 -0300

    electron

---
+++ tamagotchi/src/main.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import { createApp } from "vue";
+import App from './App.vue'
+import router from "./router";
+
+Vue.config.productionTip = false
+
+createApp(App)
+	.use(router)
+	.mount("#app");
+
+/*new Vue({
+  render: h => h(App),
+}).$mount('#app')
+*/
